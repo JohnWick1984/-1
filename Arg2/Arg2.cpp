ef6a81ceacf9289fa74723b6840d6085717c0c00
Евгений <@EUGENE1984>
2023-09-23 14:58:13 +0500

    Добавьте файлы проекта.

---
+++ Arg2/Arg2.cpp
@@ -0,0 +1,44 @@
+﻿// Arg2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cout << "Использование: " << argv[0] << " '*' C:\\Users\\Евгений\\Documents\\Учеба Академия ТОР\\ООП С++\\ООП С++\\Аргумент1\\Arg2" << std::endl;
+        return 1;
+    }
+
+    char charToReplace = argv[1][0];
+    std::string filePath = argv[2];
+
+    std::ifstream inputFile(filePath);
+    if (!inputFile) {
+        std::cerr << "Не удалось открыть файл: " << filePath << std::endl;
+        return 1;
+    }
+
+    std::string fileContents((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
+    inputFile.close();
+
+    for (char& c : fileContents) {
+        if (c == charToReplace) {
+            c = '*';
+        }
+    }
+
+    std::ofstream outputFile(filePath);
+    if (!outputFile) {
+        std::cerr << "Не удалось открыть файл для записи: " << filePath << std::endl;
+        return 1;
+    }
+
+    outputFile << fileContents;
+    outputFile.close();
+
+    std::cout << "Все вхождения символа '" << charToReplace << "' были заменены на '*'" << std::endl;
+
+    return 0;
+}
---
+++ Аргумент1/Аргумент1.cpp
@@ -0,0 +1,42 @@
+﻿// Аргумент1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main(int argc, char* argv[])
+{
+    setlocale(0, "");
+    // Проверяем, что передано правильное количество аргументов
+    if (argc != 2)
+    {
+        cerr << "Использование: " << argv[0] << "C:\\Users\\Евгений\\Documents\\Учеба Академия ТОР\\ООП С++\\ООП С++\\Аргумент1\x64\Debug\1.txt" << endl;
+        return 1; // Возвращаем ненулевой код ошибки
+    }
+
+    // Получаем путь к файлу из аргумента командной строки
+    string filePath = argv[1];
+
+    // Открываем файл для чтения
+    ifstream inputFile(filePath);
+
+    // Проверяем, удалось ли открыть файл
+    if (!inputFile.is_open()) 
+    {
+        cerr << "Не удалось открыть файл: " << filePath << endl;
+        return 2; // Возвращаем код ошибки
+    }
+
+    // Читаем и выводим содержимое файла построчно
+    string line;
+    while (getline(inputFile, line)) 
+    {
+        cout << line << endl;
+    }
+
+    // Закрываем файл
+    inputFile.close();
+
+    return 0; 
+}
